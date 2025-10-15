db48e667787ca70580a128ead74c2664464fd16b
105960769 <105960769@student.swin.edu.au>
2025-10-16 00:14:30 +1100

    feat(manage.php): added initial setup

---
+++ manage.php
@@ -0,0 +1,30 @@
+<!--
+File: Manage Dashboard 
+Author: 
+Date: Sep 29 - Oct 27 2025
+Description:
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="styles/styles.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- Establish a connection with google API -->
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> <!-- load Barlow font-->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"> <!-- Load Google icons -->
+    </head>
+    <body>
+        <?php include "./includes/header.inc"; ?>
+
+        <main>
+            <h2>Manage Dashboard</h2>
+        </main>
+
+        <?php include "./includes/footer.inc"; ?>
+    </body>
+</html>
