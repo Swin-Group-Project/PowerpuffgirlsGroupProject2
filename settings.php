3121c5b953e95aa0c05e98c2c70084e2457f8758
106060754 <106060754@student.swin.edu.au>
2025-10-20 09:30:22 +1100

    feat(apply.php): modified skills checkboxes to dynamically update with settings.php, feat(process_eoi): form data sent to process_eoi now checks if request method is post, added user input sanitisation and patterns

--- settings.php
+++ settings.php
@@ -1,6 +1,6 @@
 <!-- 
 Created: 14/10/25
-Last modified: 14/10/25
+Last modified: 20/10/25
 
 Database settings 
 Allows for connection to the database -->
@@ -9,3 +9,15 @@ Allows for connection to the database -->
     $username = "root";
     $password = ""; // Assessment requirement: Do not set a password
     $database = "project_part2";
+
+// EOI form - skills config
+$skills_data = [
+    1 => "Project Management",
+    2 => "Database Management", 
+    3 => "Front-End Development",
+    4 => "Back-End Development",
+    5 => "Lua",
+    6 => "Rust",
+    7 => "Unity",
+    8 => "Adobe Animate"
+];
