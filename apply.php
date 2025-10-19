2160e79cf242d5a92b0fa719f0a75440b7fea3e9
106060754 <106060754@student.swin.edu.au>
2025-10-20 09:30:22 +1100

    feat(apply.php): modified skills checkboxes to dynamically update with settings.php, feat(process_eoi): form data sent to process_eoi now checks if request method is post, added user input sanitisation and patterns

--- apply.php
+++ apply.php
@@ -4,6 +4,7 @@ Date created: 26/8/25
 Date modified: 7/9/25
 Author: Ryan Tay
 -->
+<?php require_once "settings.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +36,7 @@ Author: Ryan Tay
                     
                     <div id="job_reference_num_container" class="flex_item">
                         <label for="job_reference_num">Job Ref No: <span class="red_text">*</span></label>
-                        <input id="job_reference_num" name="job_reference_num" type="text" pattern="[a-zA-Z0-9]{5}" required>
+                        <input id="job_reference_num" name="job_reference_num" type="text">
                     </div>
 
                     <!-- Hoverable tooltip for job reference number -->
@@ -43,20 +44,20 @@ Author: Ryan Tay
                         <span class="material-symbols-outlined help-icon" aria-label="Show job reference number help" tabindex="0" aria-describedby="job_ref_help">help</span>
                         <span id="job_ref_help" class="help_hint" role="tooltip">The 5 alphanumberic characters in the job reference code, e.g. REF-<span class="bold">SE842</span></span>
                     </div>
-                    
+
                     <p id="job_link">You can find your <a class="bold" href="jobs.html">job reference number and other job details here.</a></p>
-                    
+
                     <div id="first_name_container" class="flex_item">
                         <label for="first_name">First Name: <span class="red_text">*</span></label>
-                        <input id="first_name" name="first_name" type="text" pattern="[a-zA-Z]{1,20}" required>
+                        <input id="first_name" name="first_name" type="text">
                     </div>
                     <div id="last_name_container" class="flex_item">
                         <label for="last_name">Last Name: <span class="red_text">*</span></label>
-                        <input id="last_name" name="last_name" type="text" pattern="[a-zA-Z]{1,20}" required>
+                        <input id="last_name" name="last_name" type="text">
                     </div>
                     <div id="date_of_birth_container" class="flex_item">
                         <label for="date_of_birth">Date of Birth: <span class="red_text">*</span></label>
-                        <input id="date_of_birth" name="date_of_birth" type="text" pattern="(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/\d{4}" placeholder="dd/mm/yyyy" required>
+                        <input id="date_of_birth" name="date_of_birth" type="text" placeholder="dd/mm/yyyy">
                     </div>
 
                     <!-- Hoverable tooltip for dd/mm/yyyy format -->
@@ -64,12 +65,12 @@ Author: Ryan Tay
                         <span class="material-symbols-outlined help-icon" aria-label="Show date of birth help" tabindex="0" aria-describedby="birth_date_help">help</span>
                         <span id="birth_date_help" class="help_hint" role="tooltip">Please enter your DOB with the format: <span class="bold">dd/mm/yyyy</span></span>
                     </div>
-                    
+
                     <fieldset id="gender_fieldset" role="group" aria-labelledby="gender_legend">
                         <legend id="gender_legend">Gender: <span class="red_text">*</span></legend>
-                        
-                        <label for="male" class="radio_container"> <!-- Nest radio buttons inside labels for accessibility (user can click label to select) -->
-                            <input id="male" value="male" name="gender" type="radio" required>
+
+                        <label for="male" class="radio_container">
+                            <input id="male" value="male" name="gender" type="radio">
                             Male
                         </label>
                         <label for="female" class="radio_container">
@@ -81,21 +82,21 @@ Author: Ryan Tay
                             Non-binary
                         </label>
                     </fieldset>
-                    
+
                     <div id="street_address_container" class="flex_item">
                         <label for="street_address">Street Address: <span class="red_text">*</span></label>
-                        <input id="street_address" name="street_address" type="text" pattern=".{1,40}" required>
+                        <input id="street_address" name="street_address" type="text">
                     </div>
 
                     <div id="suburb_town_container" class="flex_item">
                         <label for="suburb_town">Suburb/Town: <span class="red_text">*</span></label>
-                        <input id="suburb_town" name="suburb_town" type="text" pattern=".{1,40}" required>
+                        <input id="suburb_town" name="suburb_town" type="text">
                     </div>
-                    
+
                     <div id="state_container" class="flex_item">
                         <label for="state">State: <span class="red_text">*</span></label>
-                        <select id="state" required>
-                            <option value="" selected disabled>Please select</option> <!--initial 'null' option-->
+                        <select id="state">
+                            <option value="" selected disabled>Please select</option>
                             <option value="vic">VIC</option>
                             <option value="nsw">NSW</option>
                             <option value="qld">QLD</option>
@@ -106,64 +107,40 @@ Author: Ryan Tay
                             <option value="act">ACT</option>
                         </select>
                     </div>
-                    
+
                     <div id="postcode_container" class="flex_item">
                         <label for="postcode">Postcode: <span class="red_text">*</span></label>
-                        <input id="postcode" name="postcode" type="text" pattern="\d{4}" required>
+                        <input id="postcode" name="postcode" type="text">
                     </div>
                     <div id="email_container" class="flex_item">
                         <label for="email">Email: <span class="red_text">*</span></label>
-                        <input id="email" name="email" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required> <!-- (at least one char)@(at least 1 char).(at least 2 char)-->
+                        <input id="email" name="email" type="email">
                     </div>
                     <div id="phone_num_container" class="flex_item">
                         <label for="phone_num">Phone Number: <span class="red_text">*</span></label>
-                        <input id="phone_num" name="phone_num" type="text" pattern="[0-9]{8,12}" required> <!-- phone number can be 8-12 digits -->
+                        <input id="phone_num" name="phone_num" type="text">
                     </div>
 
                     <fieldset id="skills_fieldset" role="group" aria-labelledby="skills_legend">
                         <legend id="skills_legend">Skills: <span class="red_text">*</span></legend>
-                        <label for="project_management" class="checkbox_container">
-                            <input id="project_management" value="project_management" name="skills[]" type="checkbox" required>
-                            Project Management
-                        </label>
-                        <label for="database_management" class="checkbox_container">
-                            <input id="database_management" value="database_management" name="skills[]" type="checkbox">   
-                            Database Management
-                        </label>
-                        <label for="frontend" class="checkbox_container">
-                            <input id="frontend" value="frontend" name="skills[]" type="checkbox" required>
-                            Front-End Development
-                        </label>
-                        <label for="backend" class="checkbox_container">
-                            <input id="backend" value="backend" name="skills[]" type="checkbox">   
-                            Back-End Development   
-                        </label>
-                        <label for="lua" class="checkbox_container">
-                            <input id="lua" value="lua" name="skills[]" type="checkbox">
-                            Lua
-                        </label>
-                        <label for="rust" class="checkbox_container">
-                            <input id="rust" value="rust" name="skills[]" type="checkbox">
-                            Rust
-                        </label>
-                        <label for="unity" class="checkbox_container">
-                            <input id="unity" value="unity" name="skills[]" type="checkbox">
-                            Unity
-                        </label>
-                        <label for="adobe_animate" class="checkbox_container">
-                            <input id="adobe_animate" value="adobe_animate" name="skills[]" type="checkbox">
-                            Adobe Animate
-                        </label>
-                        <!-- TO DO: add dynamic accessibility attributes with PHP to indicate the container has changed state (shown/hidden)-->
+
+                        <?php foreach ($skills_data as $value => $label): ?>
+                            <label for="skill_<?php echo $value; ?>" class="checkbox_container">
+                                <input id="skill_<?php echo $value; ?>" 
+                                        value="<?php echo $value; ?>" 
+                                        name="skills[]" 
+                                        type="checkbox">
+                                <?php echo htmlspecialchars($label); ?>
+                            </label>
+                        <?php endforeach; ?>
                         <label for="other" class="checkbox_container">
                             <input id="other" value="other" name="skills[]" type="checkbox" controls="other_skills_container">
                             Other skills...
                             <div id="other_skills_container">
                                 <label for="other_skills">Enter other skills below: </label>
-                                <textarea id="other_skills" name="other_skills" rows="4"  cols="60" placeholder="Link to your portfolio, list any other relevant skills here..."></textarea>
+                                <textarea id="other_skills" name="other_skills" rows="4" cols="60" placeholder="Link to your portfolio, list any other relevant skills here..."></textarea>
                             </div>
                         </label>
-
                     </fieldset>
                     
                     <div id="button_container" class="flex_item">
--- process_eoi.php
+++ process_eoi.php
@@ -2,23 +2,15 @@
 
 // Connection to database "project_part2"
 session_start();
-require_once('settings.php');
+require_once("settings.php");
 $conn = mysqli_connect($host, $username, $password, $database);
 
-// Skill Lookup table
-$skills = [
-    'project_management',
-    'database_management',
-    'front-end_development',
-    'back-end_development',
-    'lua',
-    'rust',
-    'unity',
-    'adobe_animate'
-];
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
 
-
-$ins_stmt = $conn->prepare("INSERT INTO skills_lookup (skill_name) VALUES (?)"); // prepared statement prevents SQL injections
+// Populate skill lookup table
+$ins_stmt = $conn->prepare("INSERT INTO eoi_skill (skill_id) VALUES (?)"); // prepared statement prevents SQL injections
 
 foreach ($skills as $skill) {
     $ins_stmt->bind_param("s", $skill);
@@ -27,5 +19,48 @@ foreach ($skills as $skill) {
 
 $ins_stmt->close();
 
+function sanitize($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// Check if POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get user input
+    $job_ref_num = sanitize($_POST["job_reference_num"]);
+    $first_name = sanitize($_POST["first_name"]);
+    $last_name = sanitize($_POST["last_name"]);
+    $date_of_birth = sanitize($_POST["date_of_birth"]);
+    $gender = sanitize($_POST["gender"]);
+    $street_address = sanitize($_POST["street_address"]);
+    $suburb_town = sanitize($_POST["suburb_town"]);
+    $state = sanitize($_POST["state"]);
+    $postcode = sanitize($_POST["postcode"]);
+    $email = sanitize($_POST["email"]);
+    $phone_num = sanitize($_POST["phone_num"]);
+    $other_skills = sanitize($_POST["other_skills"]);
+    // Handle checkboxes: perform sanitize() on each item in skills, storing set items into an array
+    $skills = isset($_POST["skills"]) ? implode(", ", array_map("sanitize", $_POST["skills"])) : "";
+
+    // Validation
+    $required_errors = [];
+    $pattern_errors = [];
+    
+    $patterns = [
+        "job_reference_num" => "/^[a-zA-Z0-9]{5}$/",
+        "first_name" => "/^[a-zA-Z]{1,20}$/",
+        "last_name" => "/^[a-zA-Z]{1,20}$/",
+        "date_of_birth" => "/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/\d{4}$/",
+        "street_address" => "/^.{1,40}$/",
+        "suburb_town" => "/^.{1,40}$/",
+        "postcode" => "/^\d{4}$/",
+        "email" => "/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/",
+        "phone_num" => "/^[0-9]{8,12}$/"
+    ];
+
+    
+}
 
 ?>
