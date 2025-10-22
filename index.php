b840867adb0f4121a1f8b7150634f34c17ee8488
105960769 <105960769@student.swin.edu.au>
2025-10-22 14:18:48 +1100

    style(index.php): added inline CSS and modified some embedded CSS property values

--- index.php
+++ index.php
@@ -9,22 +9,22 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> <!-- Establish a connection with google API -->
     <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> <!-- load Barlow font-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Icons"> <!-- Load Google icons -->
-    <link rel="stylesheet" href="./styles/styles.css">
+    <link rel="stylesheet" href="styles/styles.css">
     <!-- RYAN: Example of embedded CSS: homepage tiles -->
     <style>
 
       .tile-jobs {
-        background-color: #f5f9a3;
+       background: linear-gradient(135deg, #13254b, #3b759d);
         grid-area: jobs;
       }
 
       .tile-apply {
-        background-color: #c8e8f9;
+       background: linear-gradient(135deg, #5e2f54, #bc626e);
         grid-area: apply;
       }
 
       .tile-about {
-        background-color: #fdeaff;
+        background: linear-gradient(135deg, #3b759d, #5e2f54);
         grid-area: about;
       }
 
@@ -59,16 +59,16 @@
 
         <!-- Jobs tile (links to jobs page) -->
         <a class="tile tile-jobs hover-effect" href="jobs.php" aria-label="View open jobs">
-          <p class="eyebrow">We’re hiring</p>
+          <p class="eyebrow">We&apos;re hiring</p>
           <h3>Jobs</h3>
-          <p class="tile-text">View open positions and requirements.</p>
+          <p class="tile-text" style="color: #e5d4df;">View open positions and requirements.</p>
         </a>
 
         <!-- Apply tile (links to apply page) -->
         <a class="tile tile-apply hover-effect" href="apply.php" aria-label="Apply for a role">
           <p class="eyebrow">Join us</p>
           <h3>Apply</h3>
-          <p class="tile-text">Apply by submitting your form.</p>
+          <p class="tile-text" style="color: #e5d4df;">Apply by submitting your form.</p>
           
         </a>
 
@@ -76,7 +76,7 @@
         <a class="tile tile-about hover-effect" href="about.php">
           <p class="eyebrow">Discover us</p>
           <h3>About</h3>
-          <p class="tile-text">Learn more about our team</p>
+          <p class="tile-text" style="color: #e5d4df;">Learn more about our team</p>
         </a>
 
       </section>
