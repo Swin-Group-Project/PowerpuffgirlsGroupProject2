50e21288cbca835f127be496de01a1561df22579
105960769 <105960769@student.swin.edu.au>
2025-10-23 22:26:59 +1100

    style(login): completed CSS setup for login page(includes inline+embedded)

--- login.php
+++ login.php
@@ -20,11 +20,29 @@ session_start();
         <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"> <!-- load Barlow font-->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"> <!-- Load Google icons -->
         <style>
+           .login-box {
+                background: linear-gradient(135deg, 
+                #051225 0%,     
+                #1c4159 50%,   
+                #351c38 100%   );
+            padding: 30px 50px;
+            border-radius: 12px;
+            box-shadow: 
+                0 8px 24px rgba(0,0,0,0.6),
+                0 0 30px rgba(28, 65, 89, 0.6),   
+                0 0 60px rgba(53, 28, 56, 0.4);    
+            width: 700px;
+            min-height: 500px;
+            height: auto;
+            position: relative; 
+            z-index: 1; }
         </style>
     </head>
     <body>
-    <main>
-        <div class="login-container">
+        <?php include "./includes/header.inc" ?>
+
+        <main>
+            <div class="login-container">
                 <div class="login-box">
                     <div class="login-header">
                         <img src="" alt="" class="login-logo">
@@ -32,24 +50,26 @@ session_start();
                         <p class="login-subtitle">Enter your username and password to login.</p>
                     </div>
                     <?php
-                        if (isset($_SESSION['error'])) { echo "<p style='color:red; text-align:center;'>" . $_SESSION['error'] . "</p>";    //inline css for now
-                                                        unset($_SESSION['error']);
-                                                    }
+                        if (isset($_SESSION['error'])) { 
+                            echo "<div class='error-message'>" . $_SESSION['error'] . "</div>";  
+                            unset($_SESSION['error']); }
                     ?>
                     <form action="process_login.php" method="POST" class="login-form">
                         <div class="form-group">
-                            <label for="username">Username:</label>
+                            <label for="username" class="username" style="color: white;">Username:</label>
                             <input type="text" id="username" name="username" placeholder="Enter your username" required>
                         </div>
                         <div class="form-group">
-                            <label for="password">Password:</label>
+                            <label for="password" class="password" style="color: white;">Password:</label>
                             <input type="password" id="password" name="password" placeholder="Enter your password" required >
                         </div>
                         <button type="submit" name="login" class="login-button">Login</button>
                     </form>
                 </div>
-        </div>        
-    </main>
+            </div>        
+        </main>
+
+        <?php include "./includes/footer.inc" ?>
     </body>
 </html>
 
